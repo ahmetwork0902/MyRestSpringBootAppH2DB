f9559c8d1988f9a85215d2216654bfa5b76ea9a9
ahmet <ahmetdavresh@gmail.com>
2023-11-18 11:16:36 +0300

    fix 6.4.2 lab

---
+++ src/main/java/ru/ahmetdavresh/MyRestSpringBootAppH2DB/controller/CourseController.java
@@ -0,0 +1,34 @@
+package ru.ahmetdavresh.MyRestSpringBootAppH2DB.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.ahmetdavresh.MyRestSpringBootAppH2DB.service.CourseService;
+import ru.ahmetdavresh.MyRestSpringBootAppH2DB.entity.Course;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+    @Autowired
+    private CourseService courseService;
+
+    @GetMapping
+    public List<Course> getAllCourses() {
+        return courseService.getAllCourses();
+    }
+
+    @GetMapping("/{id}")
+    public Course getCourse(@PathVariable("id") int id) {
+        return courseService.getCourse(id);
+    }
+
+    @PostMapping
+    public Course saveCourse(@RequestBody Course course) {
+        return courseService.saveCourse(course);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCourse(@PathVariable("id") int id) {
+        courseService.deleteCourse(id);
+    }
+}
